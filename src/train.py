408ffca21c42391636d1594dc7f728c6428ec1ee
Felix Sonntag <felix.sonntag@outlook.com>
2017-07-24 17:45:36 +0900

    Parallelized prediction_step

--- src/train.py
+++ src/train.py
@@ -16,45 +16,57 @@ def train_step(sess, dataset, sequence_number, model, parameters):
         if token_index in dataset.infrequent_token_indices and np.random.uniform() < 0.5:
             token_indices_sequence[i] = dataset.UNK_TOKEN_INDEX
     feed_dict = {
-      model.input_token_indices: token_indices_sequence,
-      model.input_label_indices_vector: dataset.label_vector_indices['train'][sequence_number],
-      model.input_token_character_indices: dataset.character_indices_padded['train'][sequence_number],
-      model.input_token_lengths: dataset.token_lengths['train'][sequence_number],
-      model.input_label_indices_flat: dataset.label_indices['train'][sequence_number],
-      model.dropout_keep_prob: 1-parameters['dropout_rate']
+        model.input_token_indices: token_indices_sequence,
+        model.input_label_indices_vector: dataset.label_vector_indices['train'][sequence_number],
+        model.input_token_character_indices: dataset.character_indices_padded['train'][sequence_number],
+        model.input_token_lengths: dataset.token_lengths['train'][sequence_number],
+        model.input_label_indices_flat: dataset.label_indices['train'][sequence_number],
+        model.dropout_keep_prob: 1 - parameters['dropout_rate']
     }
     _, _, loss, accuracy, transition_params_trained = sess.run(
                     [model.train_op, model.global_step, model.loss, model.accuracy, model.transition_parameters],
                     feed_dict)
     return transition_params_trained
 
-def prediction_step(sess, dataset, dataset_type, model, transition_params_trained, stats_graph_folder, epoch_number, parameters, dataset_filepaths):
+
+def prediction_step(sess, dataset, dataset_type, model, transition_params_trained, stats_graph_folder, epoch_number,
+                    parameters, dataset_filepaths):
     if dataset_type == 'deploy':
         print('Predict labels for the {0} set'.format(dataset_type))
     else:
         print('Evaluate model on the {0} set'.format(dataset_type))
     all_predictions = []
     all_y_true = []
-    output_filepath = os.path.join(stats_graph_folder, '{1:03d}_{0}.txt'.format(dataset_type,epoch_number))
+    output_filepath = os.path.join(stats_graph_folder, '{1:03d}_{0}.txt'.format(dataset_type, epoch_number))
     output_file = codecs.open(output_filepath, 'w', 'UTF-8')
     original_conll_file = codecs.open(dataset_filepaths[dataset_type], 'r', 'UTF-8')
 
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    scores_and_predictions = []
+
     for i in range(len(dataset.token_indices[dataset_type])):
         feed_dict = {
-          model.input_token_indices: dataset.token_indices[dataset_type][i],
-          model.input_token_character_indices: dataset.character_indices_padded[dataset_type][i],
-          model.input_token_lengths: dataset.token_lengths[dataset_type][i],
-          model.input_label_indices_vector: dataset.label_vector_indices[dataset_type][i],
-          model.dropout_keep_prob: 1.
+            model.input_token_indices: dataset.token_indices[dataset_type][i],
+            model.input_token_character_indices: dataset.character_indices_padded[dataset_type][i],
+            model.input_token_lengths: dataset.token_lengths[dataset_type][i],
+            model.input_label_indices_vector: dataset.label_vector_indices[dataset_type][i],
+            model.dropout_keep_prob: 1.
         }
         unary_scores, predictions = sess.run([model.unary_scores, model.predictions], feed_dict)
+        scores_and_predictions.append((unary_scores, predictions))
+
+    coord.request_stop()
+    coord.join(threads)
+
+    for i, (unary_scores, predictions) in enumerate(scores_and_predictions):
         if parameters['use_crf']:
             predictions, _ = tf.contrib.crf.viterbi_decode(unary_scores, transition_params_trained)
             predictions = predictions[1:-1]
         else:
             predictions = predictions.tolist()
 
-        assert(len(predictions) == len(dataset.tokens[dataset_type][i]))
+        assert (len(predictions) == len(dataset.tokens[dataset_type][i]))
         output_string = ''
         prediction_labels = [dataset.index_to_label[prediction] for prediction in predictions]
         gold_labels = dataset.labels[dataset_type][i]
