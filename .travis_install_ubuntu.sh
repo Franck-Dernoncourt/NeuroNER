eb77442262136e87e38ddc657499f6292bc26b95
Tom Pollard <tpollard@mit.edu>
2019-02-18 15:31:39 -0500

    add pkg management files

--- .travis_install_ubuntu.sh
+++ .travis_install_ubuntu.sh
@@ -7,15 +7,8 @@ vmstat -s
 dmidecode -t 17
 
 sudo apt-get install -y unzip
-mkdir data/word_vectors
-cd data/word_vectors
 
-# Download some word embeddings
-#wget http://nlp.stanford.edu/data/glove.6B.zip
-wget http://neuroner.com/data/word_vectors/glove.6B.100d.zip
-unzip glove.6B.100d.zip
-
-# Going back to NeuroNER src folder
-ls -la
-cd ../../src
-ls -la
+# download some word embeddings
+mkdir ./data/word_vectors
+wget -P data/word_vectors http://neuroner.com/data/word_vectors/glove.6B.100d.zip
+unzip data/word_vectors/glove.6B.100d.zip -d data/word_vectors/
