66c7498102e69e97a27f303b7b516d4743457587
Tom Pollard <tpollard@mit.edu>
2019-02-18 15:26:43 -0500

    mv test to root

--- test/test_main.py
+++ test/test_main.py
@@ -2,15 +2,16 @@
 Tests for main.py
 '''
 
-import unittest
 import os
-import main
 from shutil import rmtree
+import unittest
+
+from neuroner import neuromodel
 
 class TestMain(unittest.TestCase):
 
-    testFolder = os.path.join(os.path.dirname(__file__), "test")
-    outputFolder = os.path.join(testFolder, "output")
+    outputFolder = os.path.join('.', "output")
+    test_param_file = os.path.join('.', 'test-parameters-training.ini')
 
     def setUp(self):
         # delete the outputFolder
@@ -23,8 +24,11 @@ class TestMain(unittest.TestCase):
             rmtree(self.outputFolder)
 
     def test_ProvideOutputDir_CorrectlyOutputsToDir(self):
-        ''' Sanity test to check if all proper model output files are created in the output folder'''
-        main.main(argv=['', '--output_folder', self.outputFolder,'--parameters_filepath', os.path.join(self.testFolder, 'test-parameters-training.ini')])
+        """
+        Sanity test to check if all proper model output files are created in the output folder
+        """
+        nn = neuromodel.NeuroNER(output_folder=self.outputFolder, parameters_filepath=self.test_param_file)
+        nn.fit()
 
         # find the newest dir, from: http://stackoverflow.com/questions/2014554/find-the-newest-folder-in-a-directory-in-python
         run_outputdir = max([os.path.join(self.outputFolder,d) for d in os.listdir(self.outputFolder)], key=os.path.getmtime)
@@ -42,7 +46,3 @@ class TestMain(unittest.TestCase):
         self.assertTrue(os.path.isfile(os.path.join(run_outputdir, 'model', 'projector_config.pbtxt')))
         self.assertTrue(os.path.isfile(os.path.join(run_outputdir, 'model', 'tensorboard_metadata_characters.tsv')))
         self.assertTrue(os.path.isfile(os.path.join(run_outputdir, 'model', 'tensorboard_metadata_tokens.tsv')))
-
-if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
