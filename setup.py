76e752aa1de3f176c658b2a748185002fc5d0288
Tom Pollard <tpollard@mit.edu>
2019-10-02 19:11:46 -0400

    bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440. For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.7',
+    version='1.0.8',
 
     description='NeuroNER',
     long_description=long_description,
